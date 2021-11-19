3b032faf934e950b220da5ac0339f79590a59e00
Khaildoran <sgermscheid@gmail.com>
2021-11-19 10:07:13 -0600

    refactored into class

--- helloWorld.py
+++ helloWorld.py
@@ -1,48 +1,49 @@
 from machine import Pin, UART;
 import utime;
-import micropyGPS;
+import micropyGPS
 
+class piGPS(object):
 
-onBoardLED = Pin(25, Pin.OUT)
-gps_module = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
-print(gps_module)
+    def verifyStringID(self):
+        nmeaSentence = str(nmeastring)
+        nmeaID = nmeaSentence[3:8]
+        if nmeaID == "GPRMC":
+            return True
+        return False
 
-_gps = micropyGPS.MicropyGPS()
+    def readLine(self):
+        satSysName = gpsModule.readline()
+        return satSysName
 
+    def serialize_GPRMC(self):
+        if verifyStringID(nmeaSent):
+            for car in str(nmeaSent):
+                _gps.update(car)
+                return _gps.latitude, _gps.longitude, _gps.timestamp, _gps.date
 
-def verifyStringID(nmeastring):
-    nmeaSentence = str(nmeastring)
-    nmeaID = nmeaSentence[3:8]
-    if nmeaID == "GPRMC":
-        return True
-    return False
+    def defaultOperation(self):
+        index = 0
+        while True:
+            yield index
+            index += 1
 
-def readLine(gpsModule):
-    satSysName = gpsModule.readline()
-    return satSysName
+    def buildRuntime(self):
+        return "ope"
 
-def serialize_GPRMC(nmeaSent):
-    if verifyStringID(nmeaSent):
-        for car in str(nmeaSent):
-            _gps.update(car)
-            return _gps.latitude, _gps.longitude, _gps.timestamp, _gps.date
+        
+    def handleReset(self):
+        print("restart placeholder")
 
-def defaultOperation():
-    index = 0
-    while True:
-        yield index
-        index += 1
+    def __init__(self):
 
-def buildRuntime():
-    return
+        self.lat = 44
+        self.long = 93
+        self.onBoardLED = Pin(25, Pin.OUT)
+        self.gps_module = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+        self._gprmcReader = micropyGPS.MicropyGPS()
 
-    
-def handleReset():
-    print("restart placeholder")
-
-def __init__():
-    try:
-        buildRuntime()
-        defaultOperation()
-    except:
-        handleReset()
+        try:
+            self.buildRuntime()
+            self.defaultOperation()
+        except:
+            self.handleReset()
