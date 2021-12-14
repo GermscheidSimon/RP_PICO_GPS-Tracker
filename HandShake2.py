759dbd7dcf986d2de914ffeacf528210b1ff1661
Khaildoran <sgermscheid@gmail.com>
2021-12-14 10:30:08 -0600

    handshake service refactor

---
+++ HandShake2.py
@@ -0,0 +1,74 @@
+try :
+    from machine import Pin, UART
+    import utime
+except:
+    from serial import Serial
+    import time
+
+class Handshake(object):
+    def __init__(self):
+        self.useMicroPy = True
+        self.handShakeEstablished = False
+        
+        try :
+            import machine
+        except:
+            self.useMicroPy = False
+        
+        if self.useMicroPy:
+            self.buildMycroPySerial()
+        else:
+            self.buildCPySerial()
+
+    def requestLock(self):
+        retries = 0
+        while not self.handShakeEstablished and retries <= 100:
+            self.writeNextLine('REQACK')
+            resMsg = self.readNextLine()
+            self.RX_nextSent(resMsg)
+            if self.curRXHDR == 'RESACK':
+                self.handShakeEstablished = True
+                return True
+            retries += 1
+        return False
+    
+    def respondLock(self):
+        retries = 0
+        while self.handShakeEstablished and retries <= 100:
+            reqMsg = self.readNextLine()
+            self.RX_nextSent(reqMsg)
+            if self.curRXHDR == 'REQACK':
+                self.writeNextLine('RESACK')
+                self.handShakeEstablished = True
+                return True
+            retries += 1
+        return False
+
+    def buildMycroPySerial(self):
+        self.serialInt = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9), bits=int(8), parity=None)
+        self.readNextLine = self.serialInt.readline
+        self.writeNextLine = self.MycroPy_SerialWrite
+        
+    def MycroPy_SerialWrite(self, msg):
+        encodedMsg = str(msg).encode('utf-8')
+        self.serialInt.write(encodedMsg)
+    
+    def buildCPySerial(self):
+        self.serialInt = Serial(9600, '/dev/ttyserial0')
+        self.readNextLine = self.serialInt.read_until
+        self.writeNextLine = self.C_PySerialWrite
+
+    def C_PySerialWrite(self, msg):
+        encodedMsg = str(msg).encode('utf-8')
+        self.serialInt.write(encodedMsg)
+
+    def RX_nextSent(self, nextMsg):
+        self.curSent = nextMsg
+        startofLine = nextMsg[0] == 'b'
+        if startofLine:
+            trimmedMsg = nextMsg[2, nextMsg[-1]] # msg [b,',m,s,g,s,t,r,i,n,g,']
+            self.curRXHDR = trimmedMsg[0, 5]
+            self.curRXBDY = trimmedMsg[6, trimmedMsg[-1]]
+            return True
+        else:
+            return False
--- handshake.py
+++ handshake.py
@@ -2,7 +2,7 @@ class Handshake(object):
 
    
 
-    def __init__(self, dataMsgCount, bodySize):
+    def __init__(self, dataMsgCount, bodySize, retryThreshold):
         self.curSent = ""
         self.curRXHDR = ""
         self.curRXBDY = ""
@@ -10,6 +10,7 @@ class Handshake(object):
         self.dataMsgCount = dataMsgCount
         self.curDatMsgNbr = 0
         self.bodySize = bodySize
+        self.retryThreshold = retryThreshold
         self.handShakeEstablished = False
 
         self.HandshakeRXMSG = {
@@ -61,6 +62,12 @@ class Handshake(object):
             return f'MSGRECTRUE{curMSG}\n'
         else: 
             return f'MSGRECFALS{curMSG}\n'
+
+    def readMSGRX(self, curMsg):
+        if curMsg[6:9] == 'TRUE':
+            self.curMsgReceived = True
+        else:
+            self.curMsgReceived == False
     
     def buildDataTXR(self, msgBdy):
         datamsg = f"DATTXR{msgBdy}\n"
@@ -70,15 +77,57 @@ class Handshake(object):
         dataHdr = f'DATHDR{self.dataMsgCount}_{self.bodySize}\n'
         return dataHdr
     
-    def HandShake_TX(self):
-        currentTx_header = "INIT"
-
-        if not self.handShakeEstablished:
-            nextMsgEnum = self.HandshakeRXMSG[currentTx_header]
+    def HandShake_TX(self, retryCount, RX_callBack, dataMsgCallBack):
+        currentRX_header = "INIT"
+        while not self.handShakeEstablished and self.retryThreshold >= retryCount:
+            nextMsgEnum = self.HandshakeRXMSG[currentRX_header]
+            
             nextMsgHeader = self.HanshakeTXMSG[nextMsgEnum]
             nextMsg = self.Msg[nextMsgHeader]
             yield nextMsg
             
-        if self.handShakeEstablished:
+            if nextMsgEnum == self.HandshakeRXMSG["REQSYN"]:
+                self.handShakeEstablished = True
+                continue
 
+            msgFromRX = RX_callBack().next()
+            if not msgFromRX == None:
+                self.RX_nextSent(msgFromRX)
+                currentRX_header = self.curRXHDR
+            else: 
+                retryCount += 1
+                if retryCount == 100:
+                    raise Exception
+
+        yield self.buildDataHDR()
+
+        while self.dataMsgCount >= self.curDatMsgNbr:
+            
+            msgFromRX = RX_callBack().next()
+            self.readMSGRX(msgFromRX)
+            if self.curMsgReceived:
+                self.RX_nextSent(msgFromRX)
+                currentRX_header = self.curRXHDR
+                nextMsgBody = dataMsgCallBack(0).next()
+                self.curDatMsgNbr += 1
+                yield self.buildDataTXR(nextMsgBody)
+            else: 
+                 nextMsgBody = dataMsgCallBack(1).next()
+                 yield self.buildDataTXR(nextMsgBody)
+        
+        yield self.Msg['ENDMSG']
+        
+    def HandShake_RX(self, retryCount, RX_callBack, dataMsgCallBack):
+        while not self.handShakeEstablished and self.retryThreshold >= retryCount:
+            nextMsg = RX_callBack.next()
+            if not nextMsg == None:
+                nextMsgEnum = self.HandshakeRXMSG[nextMsg]
+                nextMsgHeader = self.HanshakeTXMSG[nextMsgEnum]
+                nextMsg = self.Msg[nextMsgHeader]
+                yield nextMsg
+            else: 
+                retryCount += 1
+                if retryCount == 100:
+                    raise Exception
+            
 
