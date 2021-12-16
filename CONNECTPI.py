6da1530ead4db393f08f9e9b47d7b05a881a0633
Khaildoran <sgermscheid@gmail.com>
2021-12-16 08:05:46 -0600

    type changes and fixes for hadnshake

--- CONNECTPI.py
+++ CONNECTPI.py
@@ -5,20 +5,23 @@ import utime
 class ConnectPi(object):
 
     def __init__(self):
-        return self
+        self.transferSuccess = False
 
     def transferCoord(self, coord):
         handshake = Handshake.Handshake()
         #encode obj to json
         jsonObj = json.dumps({"lat": f'{coord.lat}', "long": f'{coord.long}', "date": f'{coord.date}', "time": f'{coord.time}'})
-
+        self.raceCodnitionLoL()
         handshakeLock = handshake.requestLock()
         if handshakeLock:
             isSent = handshake.TX_data(jsonObj)
             if isSent:
-                return True
+                self.transferSuccess = True
+                return self.transferSuccess
             else:
-                return False
+                self.transferSuccess = False
+                return self.transferSuccess
+
     def raceCodnitionLoL(self):
         utime.sleep(2)
         print('RP TURNED ON')
