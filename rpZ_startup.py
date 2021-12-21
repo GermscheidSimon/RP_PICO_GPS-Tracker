aed003197145abe4073a7be5987e67faf28bb199
Simon Germscheid <67908776+Khaildoran@users.noreply.github.com>
2021-12-21 17:31:31 -0600

    rp zero startup file

---
+++ rpZ_startup.py
@@ -0,0 +1,15 @@
+import rpConnectZero
+
+def main():
+    try:
+        _rpPico_COM = rpConnectZero.rpConnectZero()
+        _rpPico_COM.RX_data()
+        data = _rpPico_COM.handshake.data
+        print(data)
+    except:
+        print('R2D2 Scream')
+    finally:
+        print('in theroy this is where I turn off')
+
+if __name__ == "__main__":
+    main()
