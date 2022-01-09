5bb5ebaad43472e14bfc5c3419c337b68219599a
Simon Germscheid <67908776+Khaildoran@users.noreply.github.com>
2022-01-09 12:44:08 -0600

    dotenv

--- rpZ_LTEStart.py
+++ rpZ_LTEStart.py
@@ -5,12 +5,16 @@ import asyncio
 
 class rpZ_LTEStart():
     async def __init__(self):
-        await self.start()
+        isconnected = await self.start()
+        return isconnected
     async def start(self):
-
-        await os.system("sudo ip link set wwan0 down ")
-        await os.system("echo 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip ")
-        await os.system("sudo ip link set wwan0 up")
-        await os.system("sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network=\"apn='h2g2',ip-type=4\" --client-no-release-cid")
-        await os.system("sudo udhcpc -q -f -i wwan0")
+        try:
+            await os.system("sudo ip link set wwan0 down ")
+            await os.system("echo 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip ")
+            await os.system("sudo ip link set wwan0 up")
+            await os.system("sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network=\"apn='h2g2',ip-type=4\" --client-no-release-cid")
+            await os.system("sudo udhcpc -q -f -i wwan0")
+            return True
+        except:
+            return False
         
--- rpZ_startup.py
+++ rpZ_startup.py
@@ -1,18 +1,35 @@
+from rpZ_LTEStart import rpZ_LTEStart
 import rpConnectZero
+import asyncio
+import rpZ_LTEStart
+import dotenv
+import http.client
+import json
 
 class rpZ_startup():
-    def __init__(self):
+    async def __init__(self):
         try:
-            _rpPico_COM = rpConnectZero.rpConnectZero()
-            _rpPico_COM.RX_data()
-            data = _rpPico_COM.handshake.data
-            print('great ')
-            print(data)
+            _rpZLTEConnected = await rpZ_LTEStart.rpZ_LTEStart()
+            if _rpZLTEConnected:
+                _rpPico_COM = rpConnectZero.rpConnectZero()
+                _rpPico_COM.RX_data()
+                self.data = _rpPico_COM.handshake.data
+                print('great ')
+                print(self.data)
         except:
             print('R2D2 Scream')
         finally:
-            print('in theroy this is where I turn off')
-    
-
-if __name__ == "__main__":
-    rpZ_startup()
+            conn = http.client.HTTPSConnection(dotenv.lambdaURI)
+            payload = json.dumps({
+                "id": "2",
+                "record": self.data
+            })
+            headers = {
+                'x-api-key': dotenv.xapikey,
+                'piGPS_webhook-Key': dotenv.piGPS_webhookKey,
+                'Content-Type': 'application/json'
+            }
+            conn.request("POST", "/default/piGPS_webhook", payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            print(data.decode("utf-8"))
